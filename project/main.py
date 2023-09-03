ef95142322b5dd0a910007ba02522643ce45857f
PavelZikol <zikolpavel@gmail.com>
2023-09-03 22:19:38 +0400

    Main

--- project/main.py
+++ project/main.py
@@ -1,12 +1,10 @@
-# Making imports
 from utils import (json_load, sort_last_five_list,
                                        final_information)
 
-# Assigning variables
 all_operation_list = json_load("../operations.json")
 last_five_sorted = sort_last_five_list(all_operation_list)
 final_information_list = final_information(last_five_sorted)
 
-# Printing last five formed operations
+# Вывод
 for operation in final_information_list:
     print(operation)
